68e6542d2fe6cd08f37ef0f1fbfa8a01ca05aabd
Ivan <ivansantiagopulgarleon@gmail.com>
2023-06-23 17:17:38 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import Modals from './Components/Modals';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="e1_45">
+  <div class="e1_46">
+    <div class="e1_47"></div>
+    <div class="e1_49"></div>
+    <div class="e1_50"></div>
+    <div class="e1_51"></div>
+    <div class="e1_54"></div>
+    <div class="e1_55"></div>
+    <div class="e1_56"></div>
+  </div>
+  <div class="e1_60">
+    <div class="e1_61"></div>
+    <div class="e1_62"></div><span  class="e1_63">Movies Review</span>
+    <div class="e1_64"></div>
+    <div class="e1_65"></div>
+  </div>
+  <section>
+    <Modals></Modals>
+  </section>
+</div>
   );
 }
 
